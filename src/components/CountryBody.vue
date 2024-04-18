9b2d7a5d812b999e0d5c43a46465f561eae6c2e1
ZeeksX <ikinwotezekiel@gmail.com>
2024-04-18 19:59:34 +0100

    changes

--- src/components/CountryBody.vue
+++ src/components/CountryBody.vue
@@ -1,51 +1,69 @@
 <template>
   <div class="container">
     <FilterBody @filter="updateFilter" />
-    <CardPage :filteredCountries="filteredCountries" :countriesData="countriesData" />
+    <CardPage :filteredCountries="paginatedCountries" :countriesData="countriesData" />
+    <PageNav :currentPage="currentPage" :totalPages="totalPages" @changePage="changePage" />
   </div>
 </template>
 
 <script>
 import FilterBody from './FilterBody.vue';
-
+import PageNav from './PageNav.vue';
 import CardPage from './CardPage.vue';
+
 export default {
-  name: "CountryBody",
+  name: 'CountryBody',
   components: {
     CardPage,
     FilterBody,
-
+    PageNav,
   },
   data() {
     return {
       countriesData: [],
       filteredCountries: [],
+      perPage: 20,
+      currentPage: 1,
+
     };
   },
   mounted() {
     this.getData();
   },
+  computed: {
+    paginatedCountries() {
+      const start = (this.currentPage - 1) * this.perPage;
+      const end = start + this.perPage;
+      return this.filteredCountries.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.filteredCountries.length / this.perPage);
+    },
+  },
   methods: {
     async getData() {
       try {
         if (this.countriesData.length === 0) {
-          const response = await fetch("https://restcountries.com/v3.1/all");
+          const response = await fetch('https://restcountries.com/v3.1/all');
           this.countriesData = await response.json();
         }
         this.updateFilter();
       } catch (error) {
         console.error(error);
       }
-
     },
-    updateFilter({ search = "", region = "Filter by Region" } = {}) {
+    updateFilter({ search = '', region = 'Filter by Region' } = {}) {
       this.filteredCountries = this.countriesData.filter((country) => {
         const searchMatch = country.name.common.toLowerCase().includes(search.toLowerCase());
         const regionMatch = country.region.toLowerCase().includes(region.toLowerCase());
         return searchMatch || regionMatch;
       });
+      this.currentPage = 1; // Reset current page when filter is updated
     },
-
+    changePage(page) {
+      this.currentPage = page;
+    },
   },
+
 };
 </script>
---
+++ src/components/PageNav.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="page">
+        <nav aria-label="Page navigation">
+            <ul class="pagination justify-content-center">
+                <li class="page-item" :class="{ disabled: currentPage === 1 }">
+                    <a class="page-link" href="#" @click.prevent="prevPage">Previous</a>
+                </li>
+                <li class="page-item" v-for="page in totalPages" :key="page" :class="{ active: page === currentPage }">
+                    <a class="page-link" href="#" @click.prevent="changePage(page)">{{ page }}</a>
+                </li>
+                <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+                    <a class="page-link" href="#" @click.prevent="nextPage">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentPage: Number,
+        totalPages: Number,
+    },
+    methods: {
+        changePage(page) {
+            this.$emit('changePage', page);
+        },
+        nextPage() {
+            if (this.currentPage < this.totalPages) {
+                this.$emit('changePage', this.currentPage + 1);
+            }
+        },
+        prevPage() {
+            if (this.currentPage > 1) {
+                this.$emit('changePage', this.currentPage - 1);
+            }
+        },
+    },
+};
+</script>
