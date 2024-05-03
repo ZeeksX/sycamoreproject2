a2885857ec5efad02fdb6d88d1778e660e6ae075
ZeeksX <ikinwotezekiel@gmail.com>
2024-05-03 10:56:19 +0100

    changes

---
+++ src/components/PreLoader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="preloader">
+        <img src="../assets/Spinner.svg"/>
+        <h3>Loading...</h3>
+        <p>Please Wait </p>
+    </div>
+</template>
+<script>
+export default{
+    name: "PreLoader"
+}
+</script>
+
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,65 @@
 <template>
-    <div class="home-page">
+    <div id="preloader" class="preloader" :class="{ 'loader-hidden': !preloaderVisible }">
+        <PreLoader/>
+    </div>
+    <div class="home-page" v-show="homePageVisible">
         <NavBar />
         <CountryBody />
     </div>
 </template>
+
 <script>
 import CountryBody from "@/components/CountryBody.vue";
 import NavBar from "@/components/NavBar.vue";
+import PreLoader from "@/components/PreLoader.vue";
 export default {
     name: "HomePage",
     components: {
         NavBar,
-        CountryBody
-    }
+        CountryBody,
+        PreLoader
+    },
+    data() {
+        return {
+            preloaderVisible: true,
+            homePageVisible: false
+        };
+    },
+    mounted() {
+        this.displayLoader();
+    },
+    methods: {
+        displayLoader() {
+            window.addEventListener("load", () => {
+                const loader = document.getElementById("preloader");
+                loader.classList.add("loader-hidden");
+                loader.addEventListener('transitionend', () => {
+                    loader.parentNode.removeChild(loader);
+                    this.showHomePage();
+                });
+            });
+        },
+        showHomePage() {
+            this.homePageVisible = true;
+        }
+    },
 }
 </script>
+
+<style>
+.preloader {
+    width: 100%;
+    height: 100%;
+    z-index: 9999999;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    transition: opacity 3s ease-out;
+}
+
+.loader-hidden {
+    opacity: 0;
+    pointer-events: none;
+}
+</style>
