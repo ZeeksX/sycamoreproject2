8eca90726309ab2cdd1e196c3a376dade5402198
ZeeksX <ikinwotezekiel@gmail.com>
2024-05-07 14:31:42 +0100

    changes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,59 +1,71 @@
 <template>
-    <div id="preloader" class="preloader" :class="{ 'loader-hidden': isHomePageVisible}">
-        <PreLoader />
+  <div>
+    <div id="preloader" class="preloader" :class="{ 'loader-hidden': !isLoading}">
+      <PreLoader />
     </div>
-    <div class="home-page" v-show="isHomePageVisible">
-        <NavBar />
-        <CountryBody />
+    <div class="home-page" v-show="isHomePageVisible && !isLoading">
+      <NavBar />
+      <CountryBody />
     </div>
+  </div>
 </template>
 
 <script>
+import { useCountryStore } from "@/store";
 import CountryBody from "@/components/CountryBody.vue";
 import NavBar from "@/components/NavBar.vue";
 import PreLoader from "@/components/PreLoader.vue";
 
 export default {
-    name: "HomePage",
-    components: {
-        NavBar,
-        CountryBody,
-        PreLoader
-    },
-    data() {
-        return {
-            isHomePageVisible: false,
-        };
-    },
-    created() {
-        setTimeout(() => {
-            this.isHomePageVisible = true;
-        }, 1000); 
+  name: "HomePage",
+  components: {
+    NavBar,
+    CountryBody,
+    PreLoader
+  },
+  data() {
+    return {
+      isHomePageVisible: false
+    };
+  },
+  computed: {
+    isLoading() {
+      return useCountryStore().countriesData.length < 250;
     }
-}
+  },
+  mounted() {
+    this.fetchCountriesData();
+  },
+  methods: {
+    fetchCountriesData() {
+      if (useCountryStore().countriesData.length < 250) {
+        useCountryStore().fetchData();
+      }
+      this.isHomePageVisible = true;
+    }
+  }
+};
 </script>
 
 <style>
 .preloader {
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    opacity: 1;
-    transition: opacity 0.1s ease-out;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  opacity: 1;
+ 
 }
-
 .loader-hidden {
-    display: none;
-    opacity: 0;
+  display: none;
+  opacity: 0;
 }
 .home-page{
-    transition: 3s ease-in;
+    transition: 3s ease-in
 }
 </style>
-
